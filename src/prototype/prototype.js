6671ea0597fded8adcdc8d3042925cb150fafdf6
Jason Westbrook <jwestbrook@gmail.com>
2014-04-20 19:57:15 -0700

    fix test files to point to unminified dist file
    and fix comment block

--- src/prototype/prototype.js
+++ src/prototype/prototype.js
@@ -1,12 +1,3 @@
-/*
- * Prototype JavaScript framework, version <%= PROTOTYPE_VERSION %>
- *  (c) 2005-2010 Sam Stephenson
- *
- *  Prototype is freely distributable under the terms of an MIT-style license.
- *  For details, see the Prototype web site: http://www.prototypejs.org/
- *
- *--------------------------------------------------------------------------*/
-
 /**
  * Prototype
  *
